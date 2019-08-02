ca9090b0a8bc666c0c2bede65878a655c5a86d4a
Fernando Perez-Garcia <fepegar@gmail.com>
2019-08-02 01:01:14 +0100

    Fix staple module

--- staple/staple.py
+++ staple/staple.py
@@ -23,6 +23,8 @@ class STAPLE:
             specificity_init: float = 0.99999,
             max_num_iterations: int = 1000,
             ):
+        if not arrays:
+            raise ValueError('arrays must be a list of NumPy arrays')
         self.arrays = arrays
         self.decisions_matrix = self._get_decisions_matrix(arrays)  # D
         self.maximization_first = maximization_first
@@ -30,12 +32,12 @@ class STAPLE:
         self.num_voxels, self.num_raters = self.decisions_matrix.shape  # N, R
         self.sensitivity_init = sensitivity_init
         self.specificity_init = specificity_init
-        self.sensitivity = np.empty((1, self.num_raters))  # p
-        self.specificity = np.empty((1, self.num_raters))  # q
-        one_shape = self.arrays[0].shape
-        self.output = np.empty(one_shape)  # W
-        self.prior = self.get_prior(self.decisions_matrix)
+        self.sensitivity = None  # p
+        self.specificity = None  # q
+        self.output = None  # W
+        self.prior = self._get_prior(self.decisions_matrix)
         self.max_num_iterations = max_num_iterations
+        self._init()
 
     @staticmethod
     def _get_decisions_matrix(arrays) -> np.ndarray:
@@ -48,16 +50,25 @@ class STAPLE:
         gamma = decisions_matrix.mean()
         return gamma
 
+    @staticmethod
+    def _to_column(array):
+        return array.reshape(-1, 1)
+
+    @staticmethod
+    def _to_row(array):
+        return array.reshape(1, -1)
+
     def _init(self) -> None:
-        if self.expectation_first:
+        if self.maximization_first:
+            self.output = np.array(self.arrays).mean(axis=0)
+            self.output = self._to_column(self.output)
+            self.output = self.output.astype(np.float64)
+        else:
             shape = (1, self.num_raters)
             self.sensitivity = np.full(shape, self.sensitivity_init)
             self.sensitivity = self.sensitivity.astype(np.float64)
             self.specificity = np.full(shape, self.specificity_init)
             self.specificity = self.specificity.astype(np.float64)
-        else:
-            self.output = np.array(self.arrays).mean(axis=0)
-            self.output = self.output.astype(np.float64)
         self.previous_sum = -1
         self.num_iterations = 0
 
@@ -72,21 +83,20 @@ class STAPLE:
         # Compute a
         p_matrix = p.repeat(N, axis=0)
         p1_matrix = 1 - p_matrix
-        masked_1 = np.ma.masked_array(p_matrix, D == 0).prod(axis=1)  # second term
-        masked_2 = np.ma.masked_array(p1_matrix, D == 1).prod(axis=1)  # third term
-        a = f_1 * masked_1.data * masked_2.data
-        a = a.reshape(-1, 1)  # force column
+        m_1 = np.ma.masked_array(p_matrix, D == 0).prod(axis=1)  # second term
+        m_2 = np.ma.masked_array(p1_matrix, D == 1).prod(axis=1)  # third term
+        a = f_1 * m_1.data * m_2.data
 
         # Compute b
         q_matrix = q.repeat(N, axis=0)
         q1_matrix = 1 - q_matrix
-        masked_1 = np.ma.masked_array(q_matrix, D == 1).prod(axis=1)  # second term
-        masked_2 = np.ma.masked_array(q1_matrix, D == 0).prod(axis=1)  # third term
-        b = f_0 * masked_1.data * masked_2.data
-        b = b.reshape(-1, 1)  # force column
+        m_1 = np.ma.masked_array(q_matrix, D == 1).prod(axis=1)  # second term
+        m_2 = np.ma.masked_array(q1_matrix, D == 0).prod(axis=1)  # third term
+        b = f_0 * m_1.data * m_2.data
 
         # Compute W
         W = a / (a + b)
+        W = self._to_column(W)
         self.output = W
 
     def _run_maximization(self) -> None:
@@ -99,13 +109,13 @@ class STAPLE:
         num = np.ma.masked_array(w_1_matrix, D == 0).sum(axis=0).data
         denom = w_1.sum()
         p = num / denom
-        p = p.reshape(1, -1)  # force row
+        p = self._to_row(p)
 
         w_0_matrix = w_0.repeat(R, axis=1)
         num = np.ma.masked_array(w_0_matrix, D == 1).sum(axis=0).data
         denom = w_0.sum()
         q = num / denom
-        q = q.reshape(1, -1)  # force row
+        q = self._to_row(q)
 
         self.sensitivity = p
         self.specificity = q
@@ -138,7 +148,8 @@ class STAPLE:
                 break
 
         if success:
-            return self.output
+            one_shape = self.arrays[0].shape
+            return self.output.reshape(one_shape)
         else:
             message = 'Maximum number of iterations reached before convergence'
             raise ValueError(message)
