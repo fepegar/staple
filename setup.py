44d2a2930a18c151841223660cb746de903bcc03
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-11-07 16:31:13 +0000

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fepegar/staple',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
