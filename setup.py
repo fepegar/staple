71931dbe42ff9c8ccad8867a8e7541e1e35403c9
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-11-07 16:07:23 +0000

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fepegar/staple',
-    version='0.2.3',
+    version='0.3.0',
     zip_safe=False,
 )
--- staple/__init__.py
+++ staple/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Fernando Perez-Garcia"""
 __email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 
 from .staple import *
